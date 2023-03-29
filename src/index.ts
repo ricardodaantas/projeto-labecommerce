fd7c799a7692aa2f58575a0ac90b491b802b299f
myusername <murilodesousag@gmail.com>
2023-03-29 10:22:39 -0300

    exercicio reorganizados

--- src/index.ts
+++ src/index.ts
@@ -22,16 +22,23 @@ app.listen(3003, () => {
     console.log("Servidor rodando na porta 3003");
 });
 
-app.get("/ping", (req: Request, res: Response) => {
-    res.send("Pong!");
-  });
-
-//exercicio 2
+//Users
 app.get("/users", (req: Request, res: Response)=>{
     res.status(200).send(users)
 })
 
+app.post("/users", (req: Request, res: Response)=>{
+    const id = req.body.id as string
+    const email = req.body.email as string
+    const password = req.body.password as string
 
+    const newUser:TUser = {id, email, password}
+    users.push(newUser)
+
+    res.status(201).send("Cadastro realizado com sucesso")
+})
+
+// Products
 app.get("/products", (req: Request, res: Response)=>{
     res.status(200).send(products)
 })
@@ -43,28 +50,6 @@ app.get("/products/search", (req: Request, res: Response)=>{
         return product.name === q
     })
     res.status(200).send(result)
-})
-
-app.get("/products/search", (req: Request, res: Response)=>{
-    const q = req.query.q as string
-
-    const result = products.filter((product)=>{
-        return product.name === q
-    })
-    res.status(200).send(result)
-})
-
-
-//Exercicio 3
-app.post("/users", (req: Request, res: Response)=>{
-    const id = req.body.id as string
-    const email = req.body.email as string
-    const password = req.body.password as string
-
-    const newUser:TUser = {id, email, password}
-    users.push(newUser)
-
-    res.status(201).send("Cadastro realizado com sucesso")
 })
 
 app.post("/products", (req: Request, res: Response)=>{
@@ -79,7 +64,13 @@ app.post("/products", (req: Request, res: Response)=>{
     res.status(201).send("Produto cadastrado com sucesso")
 })
 
-app.post("/purchase", (req: Request, res: Response)=>{
+
+//Purchase
+app.get("/purchases", (req: Request, res: Response)=>{
+    res.status(200).send(purchase)
+})
+
+app.post("/purchases", (req: Request, res: Response)=>{
     const userId = req.body.userId as string
     const productId = req.body.productId as string
     const quantify = req.body.quantity as number
